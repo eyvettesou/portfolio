395552062ea98bb20970c03d0535e19f2020f15b
Eyvette Sou <eyvette.sou@procore.com>
2018-05-30 00:17:07 -0700

    created header

--- src/index.js
+++ src/index.js
@@ -1,11 +1,28 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-class HelloMessage extends React.Component {
+class App extends React.Component {
   render() {
-    return <div>"Hello "{this.props.name}</div>;
+    return (
+      <div>
+        <div class="header">
+          <ul>
+            <li>
+              <a href="/">About</a>
+            </li>
+            <li>
+              <a href="/">Portfolio</a>
+            </li>
+            <li>
+              <a href="/">Contact</a>
+            </li>
+          </ul>
+        </div>
+        <div class="app">This is the body I guess</div>
+      </div>
+    );
   }
 }
 
 var mountNode = document.getElementById("app");
-ReactDOM.render(<HelloMessage name="Muthu" />, mountNode);
+ReactDOM.render(<App />, mountNode);
