63a30acfb86b7d2194bde4007987f34efbe3727b
Eyvette Sou <eyvette.sou@procore.com>
2018-06-18 23:23:08 -0700

    created page components

--- src/index.js
+++ src/index.js
@@ -3,7 +3,34 @@ import ReactDOM from 'react-dom'
 import css from './index.css'
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      container: 0,
+    }
+  }
+
   render() {
+    let currentContainer;
+
+    switch(this.state.container){
+      case 0:
+        currentContainer = <Home />;
+        break;
+      case 1:
+        currentContainer = <About />;
+        break;
+      case 2:
+        currentContainer = <Portfolio />;
+        break;
+      case 3:
+        currentContainer = <Stats />;
+        break;
+      case 4:
+        currentContainer = <Contact />;
+        break;
+    }
+
     return (
       <div>
         <div class="header">
@@ -15,14 +42,57 @@ class App extends React.Component {
               <a href="/">Portfolio</a>
             </li>
             <li>
+              <a href="/">Stats</a>
+            </li>
+            <li>
               <a href="/">Contact</a>
             </li>
           </ul>
         </div>
-        <div className={'body'}>This is the body I guess</div>
+        {currentContainer}
       </div>
     )
   }
+}
+
+const Home = () => {
+  return (
+    <div>
+      Home page.
+    </div>
+  )
+}
+
+const About = () => {
+  return (
+    <div>
+      About.
+    </div>
+  )
+}
+
+const Portfolio = () => {
+  return (
+    <div>
+      Portfolio.
+    </div>
+  )
+}
+
+const Stats = () => {
+  return (
+    <div>
+      Stats.
+    </div>
+  )
+}
+
+const Contact = () => {
+  return (
+    <div>
+      Contact.
+    </div>
+  )
 }
 
 var mountNode = document.getElementById('app')
